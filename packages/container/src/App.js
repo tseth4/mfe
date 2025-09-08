d6fffb06642e225e893c3a20612340ae9766d093
ts <tristansetha@gmail.com>
2025-09-07 20:36:25 -0500

    Marketing to Container

---
+++ packages/container/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+// import { mount } from 'marketing/MarketingApp'
+// console.log(mount)
+import MarketingApp from './components/MarketingApp';
+export default () => {
+  return (
+    <div>
+
+      <h1>Hi from container!</h1>
+      <MarketingApp />
+    </div>);
+};
